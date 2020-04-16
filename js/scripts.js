621be4b2c98f848d72aaafaca6ae92a9000f0ef0
Tristen Everett <spock214@gmail.com>
2020-04-16 11:25:07 -0700

    create function to output roman numerals for hundreds place

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,11 @@ var tensTranslate = function (number) {
   console.log("Tens place: " + number);
   return arrayTens[number];
 };
+
+var hundredsTranslate = function (number) {
+  var arrayHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+  console.log("Starting number: " + number);
+  number = parseInt(number / 100) % 10;
+  console.log("Hundreds place: " + number);
+  return arrayHundreds[number];
+};

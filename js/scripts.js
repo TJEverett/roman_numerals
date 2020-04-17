c7acb0d66307688eee29140e9d2fb7447ed5bf37
Tristen Everett <spock214@gmail.com>
2020-04-17 09:40:48 -0700

    create the submit function to take the user input and run it through the business logic and print the resulting roman numeral back to the page, also remove the console logs from the business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,34 @@
 var onesTranslate = function(number){
   var arrayOnes = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-  console.log("Starting number: " + number);
   number = parseInt(number) % 10;
-  console.log("Ones place: " + number);
   return arrayOnes[number];
 };
 
 var tensTranslate = function (number) {
   var arrayTens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-  console.log("Starting number: " + number);
   number = parseInt(number / 10) % 10;
-  console.log("Tens place: " + number);
   return arrayTens[number];
 };
 
 var hundredsTranslate = function (number) {
   var arrayHundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
-  console.log("Starting number: " + number);
   number = parseInt(number / 100) % 10;
-  console.log("Hundreds place: " + number);
   return arrayHundreds[number];
 };
 
 var thousandsTranslate = function (number) {
   var arrayThousands = ["", "M", "MM", "MMM"];
-  console.log("Starting number: " + number);
   number = parseInt(number / 1000) % 10;
-  console.log("Thousands place: " + number);
   return arrayThousands[number];
 };
+
+$(document).ready(function(){
+  $("form#numeralForm").submit(function(event){
+    event.preventDefault();
+    var arabic = parseInt($("input#number").val());
+    var roman = thousandsTranslate(arabic) + hundredsTranslate(arabic) + tensTranslate(arabic) + onesTranslate(arabic);
+    $("#arabic").text(arabic);
+    $("#roman").text(roman);
+    $("#response").show();
+  });
+});
